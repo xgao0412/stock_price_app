1f6fb53976111b82d5ad52306d7a470f050ad622
xgao420 <gaoxiangak47@gmail.com>
2018-08-14 14:04:58 -0500

    first

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect
 
 from bokeh.embed import components
@@ -87,4 +88,8 @@ def index_lulu():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	port = int(os.environ.get('PORT',5000))	
+	app.run(host='0.0.0.0',port=port)
+
+
+
